05ce23f998b2ef4c6031926eeadf4331688972ea
Matheus <matheus_maschietto@yahoo.com.br>
2022-06-28 16:32:14 -0300

    MODELS

--- projeto0/recipes/models.py
+++ projeto0/recipes/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
+
+
+class Recipe(models.Model):
+    tittle = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
 
 # Create your models here.

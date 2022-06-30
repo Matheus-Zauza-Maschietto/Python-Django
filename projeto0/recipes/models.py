e83d08811db97ebde566cc7db276389ebfe7eaf5
Matheus <matheus_maschietto@yahoo.com.br>
2022-06-30 11:17:38 -0300

    update banco de dados usando modals

--- projeto0/recipes/models.py
+++ projeto0/recipes/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from pyexpat import model
 from django.db import models
 
@@ -6,5 +7,15 @@ class Recipe(models.Model):
     tittle = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
+    preparation_time = models.SmallIntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.SmallIntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.DateTimeField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
 
 # Create your models here.

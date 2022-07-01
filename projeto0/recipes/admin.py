05389ed1a23f12f80686d32326d486c0b4418ed6
Matheus <matheus_maschietto@yahoo.com.br>
2022-07-01 10:23:01 -0300

    update partição admin do site

--- projeto0/recipes/admin.py
+++ projeto0/recipes/admin.py
@@ -1,3 +1,17 @@
+from unicodedata import category
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category, CategoryAdmin)
--- projeto0/recipes/models.py
+++ projeto0/recipes/models.py
@@ -1,6 +1,15 @@
 from distutils.command.upload import upload
 from pyexpat import model
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -15,7 +24,14 @@ class Recipe(models.Model):
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.DateTimeField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='projeto0/recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.tittle
+
 
 # Create your models here.

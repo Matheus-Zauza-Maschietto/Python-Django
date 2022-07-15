7a146b8e67aa8267e08647a6050d3bb2a8481938
Matheus <matheus_maschietto@yahoo.com.br>
2022-07-15 14:32:20 -0300

    update

--- projeto0/recipes/views.py
+++ projeto0/recipes/views.py
@@ -6,25 +6,12 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(is_published=True).order_by('-id'))
     return render(request, 'recipes/pages/home.html', status=200, context={'recipes': recipes})
 
 
 def category(request, category_id):
-    '''
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('not found')
-
-        Outros modos de encaminhar um erro 404
-        1° Usando getattr dentro de getattr
-        category_name = getattr(
-        getattr(recipes.first(), 'category', None), 'name', 'not found')
-        2° Usando  
-    else:
-        '''
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id, is_published=True).order_by('-id'))
@@ -32,7 +19,8 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': make_recipe(), 'is_detail_page': True, })
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': recipe, 'is_detail_page': True, })
 
 
 def teste(request):

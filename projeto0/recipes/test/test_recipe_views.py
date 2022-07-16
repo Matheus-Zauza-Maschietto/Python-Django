7dd5c401f471e27129971e8e6e0f5122196c520d
Matheus <matheus_maschietto@yahoo.com.br>
2022-07-16 20:37:26 -0300

    ypdate test

--- projeto0/recipes/test/test_recipe_views.py
+++ projeto0/recipes/test/test_recipe_views.py
@@ -20,10 +20,15 @@ class RecipeViewsTeste(TestCase):
         view = resolve(reverse('recipes:teste'))
         self.assertIs(view.func, views.teste)
 
-    def test_recipe_home_view_returns_status_code_200_OK(self):
+    '''def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'), secure=True)
+        self.assertTemplateUsed(response, 'recipes.views.home')
+'''
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+        self.assertIn('No recipes found', response.content.decode('utf-8'),)
